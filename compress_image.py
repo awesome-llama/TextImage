7e0c6ce48b24474c0aa8c929f0699e904d1c7d3e
unyxium <unyxium@gmail.com>
2023-10-22 03:47:10 +1100

    fixes

--- compress_image.py
+++ compress_image.py
@@ -5,7 +5,9 @@ import numpy as np
 import layer_RGB8
 #import layer_A8
 
-debug_dimensions = ()
+debug_dimensions = () # TESTING ONLY
+
+# TODO: create proper functions for handling file loading and saving.
 
 def compress_img(image_name, path='', lossy_tolerance=0):
     """Open an image and compress it. Save."""
@@ -26,32 +28,29 @@ def compress_img(image_name, path='', lossy_tolerance=0):
         f.write(file)
 
 
-##############
 
 def decompress_img(image_name, path=''):
     
     with open(path+image_name) as f:
         image = f.read()
 
-    
-    dimensions = (256, 256)
-
-    print(debug_dimensions)
+    #dimensions = (256, 256)
+    #print(debug_dimensions)
     dimensions = debug_dimensions
+    
     image_array = layer_RGB8.decompress(image, dimensions)
 
     # output the image:
     image_array = np.array(image_array, np.uint8)
     image_array = np.flip(image_array.reshape(dimensions[1], dimensions[0], 3), 0)
-
     image = Image.fromarray(obj=image_array, mode='RGB')
+
     image.show()
     image.save(path + 'decompressed.png')
 
 
 if __name__ == '__main__':
 
-    #compress_img('images/beach_hq.png', lossy_tolerance=0)
     compress_img('images/beach.png')
     #compress_img('images/test.png')
     #compress_img('images/grad.png', lossy_tolerance=20)
@@ -60,10 +59,7 @@ if __name__ == '__main__':
     #compress_img('images/testcard.png')
     #compress_img('images/breakable_atlas.png')
     #compress_img('images/nprguy.jpg')
-    #compress_img('images/rgb8.png')
     #compress_img('images/region_pod.png')
-    #compress_img('images/rhodes noisy.png')
     #compress_img('images/alltrue_mc_textures.png')
-    #compress_img('images/lvl3.png')
     
-    decompress_img('compressed.txt')
+    decompress_img('compressed.txt') # decompress the image
