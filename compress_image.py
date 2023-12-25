0e3961630edd4e1afb8809f7911996a32021e3dc
unyxium <unyxium@gmail.com>
2023-12-26 03:19:57 +1100

    changed path handling

--- compress_image.py
+++ compress_image.py
@@ -7,38 +7,39 @@ import layer_RGB8
 
 debug_dimensions = () # TESTING ONLY
 
-# TODO: create proper functions for handling file loading and saving.
+# TODO: create proper functions for handling file loading and saving, handle the file paths better too
 
-def compress_img(image_name, path='', lossy_tolerance=0):
+def compress_img(image_path, output_path='output/', lossy_tolerance=0, debug=False):
     """Open an image and compress it. Save."""
     
-    image = Image.open(path+image_name)
+    image = Image.open(image_path)
+    
+    image = image.convert('RGB') # TODO detect alpha
     image_array = np.asarray(image)
     image_array = np.flip(image_array, 0)
     image_array = np.reshape(image_array, (image.width*image.height, 3), 'A')
     global debug_dimensions
     debug_dimensions = image.size
     
-    data_stream = layer_RGB8.compress(image_array, image.size, lossy_tolerance)
+    data_stream = layer_RGB8.compress(image_array, image.size, lossy_tolerance, debug=debug)
 
     file = data_stream
-    #file = f"img,v:0.1,w:{image.width},h:{image.height},layers:1#len:{len(data_stream)}|{data_stream}"
 
-    with open(f'{path}compressed.txt', 'w') as f:
+    with open(output_path + 'compressed.txt', 'w') as f:
         f.write(file)
 
 
 
-def decompress_img(image_name, path=''):
+def decompress_img(image_path, output_path='output/', debug=False):
     
-    with open(path+image_name) as f:
+    with open(image_path) as f:
         image = f.read()
 
     #dimensions = (256, 256)
     #print(debug_dimensions)
     dimensions = debug_dimensions
     
-    image_array = layer_RGB8.decompress(image, dimensions)
+    image_array = layer_RGB8.decompress(image, dimensions, debug=debug)
 
     # output the image:
     image_array = np.array(image_array, np.uint8)
@@ -46,10 +47,11 @@ def decompress_img(image_name, path=''):
     image = Image.fromarray(obj=image_array, mode='RGB')
 
     image.show()
-    image.save(path + 'decompressed.png')
+    image.save(output_path + 'decompressed.png')
 
 
 if __name__ == '__main__':
+    print('compress')
 
     #compress_img('images/beach.png')
     #compress_img('images/test.png')
@@ -58,8 +60,10 @@ if __name__ == '__main__':
     #compress_img('images/boat.png', lossy_tolerance=1)
     #compress_img('images/testcard.png')
     #compress_img('images/breakable_atlas.png')
-    compress_img('images/nprguy.jpg')
+    #compress_img('images/nprguy.jpg')
+    #compress_img('C:/Users/Atlas/Downloads/col.bmp')
     #compress_img('images/region_pod.png')
-    #compress_img('images/alltrue_mc_textures.png')
+    compress_img('C:/Users/Atlas/Documents/Scratch/Image Format 2/all images/alltrue_mc_textures.png', lossy_tolerance=0, debug=True)
     
-    decompress_img('compressed.txt') # decompress the image
+    print('decompress')
+    decompress_img('output/compressed.txt', debug=True) # decompress the image
