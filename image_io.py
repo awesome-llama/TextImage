a0c9509157e34afc4881c9fc125f503d7849b3f7
unyxium <unyxium@gmail.com>
2023-12-28 03:43:54 +1100

    prep

--- image_io.py
+++ image_io.py
@@ -5,3 +5,8 @@ from PIL import Image
 import numpy as np
 import layer_A8
 import layer_RGB8
+
+
+def compress_img(image_path, output_path, mode='RGB', lossy_tolerance=0, debug=False):
+    pass
+    # choose where to load and save, specify colour mode, compression, debug, 
