2461fddffcfd017dd88c5341ee749d0df247e721
unyxium <unyxium@gmail.com>
2024-02-03 02:45:54 +1100

    save preview as file

--- quick_preview.py
+++ quick_preview.py
@@ -7,6 +7,10 @@ pasted = pyperclip.paste()
 if pasted[0:6] == 'txtimg':
     txtimg = image_io.load_from_text(pasted)
     img = txtimg.to_pillow_image(require_alpha=False)
+    try:
+        img.save('output/clipboard.png')
+    except Exception as e:
+        print(e)
     img.show()
 else:
     print('no TextImage detected in clipboard')
